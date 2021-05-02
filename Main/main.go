584c13814c7c2a3a46041298d8444bcc7fa05a92
mcgizmo26 <lonnie.mcgill@smartshyp.com>
2021-05-02 15:00:28 -0600

    Adding comments to the main.go file

--- Main/main.go
+++ Main/main.go
@@ -2,9 +2,19 @@ package main
 
 import "fmt"
 
+/*
+	Just a simple little excercise to keep how loop through a go slice check
+	if it is even or odd using modulus and print it using format and print
+	from Go's standard library.
+*/
+
 func main() {
+
+	// New int slice is created and initialized to the numSlice variable.
 	numSlice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
+	// Loop through the numSlice slice and determine if it is even or odd
+	// using modlus and the print the results.
 	for i := 0; i < len(numSlice); i++ {
 		if i%2 == 0 {
 			fmt.Printf("%v is even!!\n", numSlice[i])
